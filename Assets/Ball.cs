acf4672e6271ebbf4392fa603cb938a1df7375a0
yoo-s <soomin.yoo95@gmail.com>
2015-08-28 18:27:47 -0700

    Add pong game

---
+++ Assets/Ball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public float speed = 30;
+
+	// Use this for initialization
+	void Start () {
+		// Initial velocity
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		// col holds collision info; if ball collides with racket then:
+		// racket = col.gameObject
+		// racket position = col.transform.position
+		// racket collider = col.collider
+
+}
---
+++ Assets/MoveRacket2.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket2 : MonoBehaviour {
+	public float speed = 30;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float v = Input.GetAxisRaw ("Vertical2");
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, v) * speed;
+		
+	}
+}
